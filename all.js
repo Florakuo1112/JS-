aa98c9740ccf4d0755778bc6148e89fe1d4d5e66
Flora Kuo In The House <tingyu1112@gmail.com>
2023-12-19 17:50:07 +0800

    使用validte.js套件來做表單驗證功能

--- all.js
+++ all.js
@@ -20,7 +20,68 @@ const ticketPrice_message = document.querySelector("#ticketPrice-message");
 const ticketNum_message = document.querySelector("#ticketNum-message");
 const ticketRate_message= document.querySelector("#ticketRate-message");
 const desRequired = document.querySelector(".desRequired");
+//for validate.js的綁定
 const addTicketform = document.querySelector(".addTicket-form");
+const input = addTicketform.querySelectorAll('input, select, textarea');
+
+
+
+//for validate.js 設定條件
+const constraints = {
+  套票名稱:{
+      presence:{
+        allowEmpty:false,
+        message:"必填"
+      }
+  },
+  圖片網址:{
+      presence:{
+        allowEmpty:false,
+        message:"必填"
+      },
+      url:{
+        message:"非有效網址"
+      }
+  },
+  景點地區:{
+      presence:{
+        allowEmpty:false,
+        message:"必填"
+      }
+  },
+  套票金額:{
+      presence:{
+        allowEmpty:false,
+        message:"必填"
+      }
+  },
+  套票組數:{
+      presence:{
+        allowEmpty:false,
+        message:"必填"
+      }
+  },
+  套票星級:{
+      presence:{
+        allowEmpty:false,
+        message:"必填"
+      }
+  },
+  套票描述:{
+      presence:{
+        allowEmpty:false,
+        message:"必填"
+      },
+      length:{
+        is:5,
+        message: `字數等於5`
+      }
+  }
+};
+
+
+
+
 //查無此關鍵字資料
 const cantFindArea = document.querySelector(".cantFind-area");
 
@@ -112,80 +173,55 @@ regionSearch.addEventListener("change", function(e){
 
   //增加票券功能
 addTicket.addEventListener("click",function(e){
-  let obj = {};
+  console.log(input);
+
+  //先把alart message變成空白
+  input.forEach((item)=>{
+    if(item.value == "新增套票"){
+      return
+    };
+    let eachNextElement = item.parentElement.nextElementSibling
+  //  console.log(eachNextElement);
+    eachNextElement.innerHTML =  
+    `<p id="ticketName-message" data-message="套票名稱">
+  <i class="fas fa-exclamation-circle"></i>
+           <span></span>
+     </p>`
+
+  });
+
+  //把輸入的資訊存在dataToValidate
+  let dataToValidate ={};
+  input.forEach((item)=>{
+    dataToValidate[item.name] = item.value;
+  });
+
+  console.log(dataToValidate);
+
+const errors = validate(dataToValidate, constraints); 
+//const errors = validate(dataToValidate, constraints, {format: 'flat' }); 
+console.log(errors);
+if(errors !== undefined){
+  input.forEach((item)=>{
+    if(errors[item.name] !== undefined){
+    console.log(errors[item.name][0].split(' '));
   
-
-//先確認有沒有未填寫的input item, 如果有 missInputItem +=1;
-let inputItem = [ticketName, ticketImgUrl, ticketRegion, ticketPrice, ticketNum, ticketRate, ticketDescription];
-let missInputItem = 0;
-
-inputItem.forEach(function(item){
-  if(item.value === ""){
-    missInputItem += 1;
-  }
-});
-
-console.log(`有${missInputItem}項inputItem沒有填寫`);
-
-if(missInputItem > 0){
-  //如果有未填寫的inputItem, 單純做頁面上的渲染
-  //套票名稱
-  if(ticketName.value === ""){
-    requiredRender(ticketName_message);
-  }else{
-     noRequire(ticketName_message)
-  };
-  //圖片網址
-  if(ticketImgUrl.value === ""){
-    requiredRender(ticketImgUrl＿message);
-  }else{
-    noRequire(ticketImgUrl＿message)
-  };
-  //景點地區
-  if(ticketRegion.value === ""){
-    requiredRender(ticketRegion_message);
-  }else{
-    noRequire(ticketRegion_message)
-  };
-  //套票金額
-  if(ticketPrice.value === ""){
-    requiredRender(ticketPrice_message);
-  }else{
-    noRequire(ticketPrice_message)
-  };
-  //套票組數
-  if(ticketNum.value === ""){
-    requiredRender(ticketNum_message);
-  }else{
-    noRequire(ticketNum_message)
-  };
-  //套票星級
-  if(ticketRate.value === ""){
-    requiredRender(ticketRate_message);
-  }else{
-    noRequire(ticketRate_message)
-  };
-  //套票描述
-  if(ticketDescription.value === ""){
-    requiredRender(desRequired);
-  }else{
-    noRequire(desRequired)
-  };
-
-  //都填寫了，跑以下程式碼
-} else{
-  //把必填紅字給拿掉，如果不加以下程式碼會造成原本出現的必填紅字在新增成功後還是會留在頁面上
-  noRequire(ticketName_message);
-  noRequire(ticketImgUrl＿message);
-  noRequire(ticketRegion_message);
-  noRequire(ticketPrice_message);
-  noRequire(ticketNum_message);
-  noRequire(ticketRate_message);
-  noRequire(desRequired);
-  //選單回到全部地區
+    let eachNextElement = item.parentElement.nextElementSibling
+    console.log(eachNextElement);
+    eachNextElement.innerHTML =  
+    `<p id="ticketName-message" data-message="套票名稱">
+    <i class="fas fa-exclamation-circle"></i>
+             <span>${errors[item.name][0].split(' ')[1]}</span>
+       </p>`
+    //
+    }
+  })
+}else{
+  console.log("都有填寫");
   regionSearch.value = "" 
   console.log("all sets");
   //push 到data
+  let obj = {};
   obj.id = data.length+1
   obj.name = ticketName.value;
   obj.imgUrl=ticketImgUrl.value;
@@ -200,7 +236,99 @@ if(missInputItem > 0){
   render(data);
   addTicketform.reset();
   donuteChart()
+
 }
+
+
+
+//以下是不使用validate.js, 土法煉鋼的方法
+//先確認有沒有未填寫的input item, 如果有 missInputItem +=1;
+// let inputItem = [ticketName, ticketImgUrl, ticketRegion, ticketPrice, ticketNum, ticketRate, ticketDescription];
+// let missInputItem = 0;
+
+// inputItem.forEach(function(item){
+//   if(item.value === ""){
+//     missInputItem += 1;
+//   }
+// });
+
+// console.log(`有${missInputItem}項inputItem沒有填寫`);
+
+// if(missInputItem > 0){
+//   //如果有未填寫的inputItem, 單純做頁面上的渲染
+//   //套票名稱
+//   if(ticketName.value === ""){
+//     requiredRender(ticketName_message);
+//   }else{
+//      noRequire(ticketName_message)
+//   };
+//   //圖片網址
+//   if(ticketImgUrl.value === ""){
+//     requiredRender(ticketImgUrl＿message);
+//   }else{
+//     noRequire(ticketImgUrl＿message)
+//   };
+//   //景點地區
+//   if(ticketRegion.value === ""){
+//     requiredRender(ticketRegion_message);
+//   }else{
+//     noRequire(ticketRegion_message)
+//   };
+//   //套票金額
+//   if(ticketPrice.value === ""){
+//     requiredRender(ticketPrice_message);
+//   }else{
+//     noRequire(ticketPrice_message)
+//   };
+//   //套票組數
+//   if(ticketNum.value === ""){
+//     requiredRender(ticketNum_message);
+//   }else{
+//     noRequire(ticketNum_message)
+//   };
+//   //套票星級
+//   if(ticketRate.value === ""){
+//     requiredRender(ticketRate_message);
+//   }else{
+//     noRequire(ticketRate_message)
+//   };
+//   //套票描述
+//   if(ticketDescription.value === ""){
+//     requiredRender(desRequired);
+//   }else{
+//     noRequire(desRequired)
+//   };
+
+//   //都填寫了，跑以下程式碼
+// } else{
+//   //把必填紅字給拿掉，如果不加以下程式碼會造成原本出現的必填紅字在新增成功後還是會留在頁面上
+//   noRequire(ticketName_message);
+//   noRequire(ticketImgUrl＿message);
+//   noRequire(ticketRegion_message);
+//   noRequire(ticketPrice_message);
+//   noRequire(ticketNum_message);
+//   noRequire(ticketRate_message);
+//   noRequire(desRequired);
+//   //選單回到全部地區
+  // regionSearch.value = "" 
+  // console.log("all sets");
+  // //push 到data
+  // obj.id = data.length+1
+  // obj.name = ticketName.value;
+  // obj.imgUrl=ticketImgUrl.value;
+  // obj.area = ticketRegion.value;
+  // obj.price = ticketPrice.value;
+  // obj.group = ticketNum.value;
+  // obj.rate = ticketRate.value;
+  // obj.description = ticketDescription.value;
+  // data.push(obj);
+  // console.log(obj)
+  // console.log(regionSearch.value);
+  // render(data);
+  // addTicketform.reset();
+  // donuteChart()
+// }
+
 
 });
 
